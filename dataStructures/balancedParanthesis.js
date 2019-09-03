80f54500f1fb26c730c2acff173578a89dc06f42
indrajeetgajbhiye <indrajeetgajbhiye@gmail.com>
2019-09-03 14:42:44 +0530

    Add eslint automation

--- dataStructures/balancedParanthesis.js
+++ dataStructures/balancedParanthesis.js
@@ -1,4 +1,34 @@
+/******************************************************************************
+ * Execution    :   default node            terminal> node cashCounter.js
+ * 
+ * Purpose      :   to balance the paranthesis of given expression and return true if balanced
+ *                  .
+ * 
+ * @description
+ * 
+ * @file        :   balanceParanthesis.js
+ * @overview    :   to balance the paranthesis in given arithmatic expression
+ *                  
+ * @author      :   Indrajeet Gajbhiye indrajeetgajbhiye@gmail.com
+ * @version     :   1.0
+ * @since       :   30-08-2019
+*******************************************************************************/
+/**
+ * @var {*} balanceUtils 
+ * @description to get the contecnt from the file 
+ */
 var balanceutils = require('../utility/balanceUtils');
+/**
+ * @var {*} common 
+ * @description to access the common util functions
+ */
 var common = require('../utility/commonUtils');
+/**
+ * @var {string} expression
+ * @description to read the string fro user
+ */
 var expression = common.inputFunctionString('Enter the expression');
+/**
+ * @description calling the function main(expression) in balanceUtils
+ */
 balanceutils.main(expression);
--- dataStructures/bankingCounter.js
+++ dataStructures/bankingCounter.js
@@ -1,3 +1,95 @@
-var banking = require('../utility/bankingCounterUtils');
-var bankingObj = new banking.Bank(10000, 4);
-bankingObj.processBanking();
+/******************************************************************************
+ * Execution    :   default node            terminal> node cashCounter.js
+ * 
+ * Purpose      :   To enter amounts of people in queue and deposit/withdraw amount from bank
+ *                  and dequeue them after transaction.
+ * 
+ * @description
+ * 
+ * @file        :   bankingCounter.js
+ * @overview    :   To enter amounts of people in queue and deposit/withdraw amount from bank
+ *                  and dequeue them after transaction.
+ * @author      :   Indrajeet Gajbhiye indrajeetgajbhiye@gmail.com
+ * @version     :   1.0
+ * @since       :   30-08-2019
+*******************************************************************************/
+
+const read = require('readline-sync');
+const cashUtil = require('../utility/cashCountUtility');
+var amount = parseInt(50000);
+var min = 10000;
+while(amount>min)
+{
+    var n = cashUtil.numInput();        //number of people entering the queue at a time
+    var arr = [];
+    console.log("Enter the amounts of people: ");
+    for(var i=0; i<n; i++)
+    {
+        arr[i] = parseInt(read.question(""));
+    }
+    var head = new cashUtil.queueNode(null);
+    var temp = head;
+    for(var i=0; i<n; i++)
+    {
+        temp = cashUtil.push(temp, head, arr, i);
+    }
+    var current = head;
+    console.log("Queue : ");
+    cashUtil.print(current);
+    var temp = head;
+    for(var i=0; i<n; i++)
+    {
+        console.log("person ", (i+1) , "'s amount : " , temp.data);
+        var num = parseInt(read.question("Press 1 for deposit and 2 for withdrawl: "));
+        if(num === 1)
+        {
+            amount = amount+temp.data;
+        }
+        else if(num === 2)
+        {
+            if(temp.data < amount)
+            {
+                amount = amount-temp.data;
+            }
+            else if(amount-temp.data < min)
+            {
+                console.log("Insufficient funds in bank");
+                console.log("Amount in bank : ",amount);
+                var current = head;
+                console.log("Queue : ");
+                cashUtil.print(current);
+                temp = temp.next;
+                head = head.next;
+                break;
+            }
+            else
+            {
+                console.log("Insufficient funds in bank");
+                console.log("Amount in bank : ",amount);
+                var current = head;
+                console.log("Queue : ");
+                cashUtil.print(current);
+                temp = temp.next;
+                head = head.next;
+                break;
+            }
+        }
+        temp = temp.next;
+        head = head.next;
+        console.log("Available amount in bank : " + amount);
+        var current = head;
+        console.log("Queue : ");
+        cashUtil.print(current);
+    }
+    console.log("Type 'yes' if you want to quit the queue else any input to continue");
+    var ans = read.question("");
+    if(ans === 'yes')
+    {
+        console.log("Final amount is bank : ",amount);
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
