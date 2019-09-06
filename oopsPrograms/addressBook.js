8f9e15a6e744d53138492ec9f5c91fc5151eb426
indrajeetgajbhiye <indrajeetgajbhiye@gmail.com>
2019-09-06 18:12:50 +0530

    day end

---
+++ oopsPrograms/addressBook.js
@@ -0,0 +1,348 @@
+var readlineSync = require('readline-sync');
+var fs = require('fs');
+var data = fs.readFileSync('../OopsPrograms/JSONfiles/addressBook.json');
+var addressb = JSON.parse(data);
+var name, lastname, street, city, state, nation, zip, phoneNum;
+nameRestriction = /[a-z]/ig;
+contactRestriction = /[0-9]/g;
+function check() {
+    name = readlineSync.question("Please enter your first name: ");
+    if (nameRestriction.test(name) == false) {
+        console.log("Invalid name!");
+        return false;
+    }
+    lastName = readlineSync.question("Please enter your last name: ");
+    if (nameRestriction.test(lastName) == false) {
+        console.log("Invalid name!");
+        return false;
+    }
+    console.log("***Address Info***");
+    street = readlineSync.question("Street: ");
+    city = readlineSync.question("City: ");
+    if (nameRestriction.test(city) == false) {
+        console.log("Invalid city name!");
+        return false;
+    }
+    state = readlineSync.question("State: ");
+    if (nameRestriction.test(state) == false) {
+        console.log("Invalid state name!");
+        return false;
+    }
+    nation = readlineSync.question("Nationality: ");
+    if (nameRestriction.test(nation) == false) {
+        console.log("Invalid Nationality!");
+        return false;
+    }
+    zip = readlineSync.question("Zip code: ");
+    if (contactRestriction.test(zip) == false || zip.lenght != 6) {
+        console.log("Invalid zip code!");
+        return false;
+    }
+    phoneNum = readlineSync.question("Phone number: ");
+    if (contactRestriction.test(phoneNum) == false || phoneNum.length != 10) {
+        console.log("Invalid phone number!");
+        return false;
+}
+class Address {
+
+    constructor() {
+        this.addressb;
+    }
+    createAddress() {
+        var name = readlineSync.question("Please enter your first name: ");
+        if (nameRestriction.test(name) == false) {
+            console.log("Invalid name!");
+            return false;
+        }
+        var lastName = readlineSync.question("Please enter your last name: ");
+        if (nameRestriction.test(lastName) == false) {
+            console.log("Invalid name!");
+            return false;
+        }
+        console.log("***Address Info***");
+        var street = readlineSync.question("Street: ");
+        var city = readlineSync.question("City: ");
+        if (nameRestriction.test(city) == false) {
+            console.log("Invalid city name!");
+            return false;
+        }
+        var state = readlineSync.question("State: ");
+        if (nameRestriction.test(state) == false) {
+            console.log("Invalid state name!");
+            return false;
+        }
+        var nation = readlineSync.question("Nationality: ");
+        if (nameRestriction.test(nation) == false) {
+            console.log("Invalid Nationality!");
+            return false;
+        }
+        var zip = readlineSync.question("Zip code: ");
+        if (contactRestriction.test(zip) == false || zip.lenght != 6) {
+            console.log("Invalid zip code!");
+            return false;
+        }
+        var phoneNum = readlineSync.question("Phone number: ");
+        if (contactRestriction.test(phoneNum) == false || phoneNum.length != 10) {
+            console.log("Invalid phone number!");
+            return false;
+        }
+        addressb.Person.push({
+            "Name": name,
+            "LastName": lastName,
+            "Address": {
+                "Street": street,
+                "City": city,
+                "State": state,
+                "Nationality": nation,
+                "Zip": zip,
+                "PhoneNum": phoneNum
+            }
+        })
+        fs.writeFile('addressBook1.json', JSON.stringify(addressb), 'utf-8', function (err) {
+            if (err) throw err
+            console.log('Done!');
+        })
+        console.log("Address updated succesfully!");
+        console.log("Your information as per our record is: \r\n First Name: " + name + "\r\nLast Name: " + lastName + "\r\nStreet: " + street + "\r\nCity: " + city + "\r\nState: " + state + "\r\nNationality: " + nation);
+    }
+    compare1(a, b) {
+        if (a.Name.toLowerCase() < b.Name.toLowerCase()) {
+            return -1;
+        }
+        if (a.Name.toLowerCase() > b.Name.toLowerCase()) {
+            return 1;
+        }
+        return 0;
+    }
+    sorting() {
+        console.log(addressb.Person.sort(this.compare1));
+    }
+    openProfile() {
+        var temp = -1;
+        if (addressb.Person.length > 0) {
+            for (var i = 0; i < addressb.Person.length; i++) {
+                console.log(addressb.Person[i]);
+            }
+            console.log("Welcome!!");
+            var update = readlineSync.question("Please enter the name of profile: ");
+            for (var k = 0; k < addressb.Person.length; k++) {
+                if (update == addressb.Person[k].Name) {
+                    temp = k;
+                    console.log("What you want to do?");
+                    console.log("1: Update");
+                    console.log("2: Delete");
+                    console.log("3: Sort");
+                    console.log("4: Save");
+                    console.log("5: Exit");
+                    var choice2 = readlineSync.question("Please enter your choice: ");
+                    switch (parseInt(choice2)) {
+                        case 1:
+                            console.log("What do you want to update? ");
+                            console.log("1: Street");
+                            console.log("2: City");
+                            console.log("3: State");
+                            console.log("4: Nationality");
+                            console.log("5: Zip code");
+                            console.log("6: Phone");
+                            console.log("7: Exit");
+                            var choice3 = readlineSync.question("Please enter your choice: ");
+                            switch (parseInt(choice3)) {
+                                case 1:
+                                    var sUpdate = readlineSync.question("Enter the new street number: ");
+                                    if (nameRestriction.test(sUpdate) == false && sUpdate.length > 3) {
+                                        console.log("Invalid name!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": sUpdate,
+                                            "City": addressb.Person[k]["Address"].City,
+                                            "State": addressb.Person[k]["Address"].State,
+                                            "Nationality": addressb.Person[k]["Address"].Nation,
+                                            "Zip": addressb.Person[k]["Address"].Zip,
+                                            "PhoneNum": addressb.Person[k]["Address"].PhoneNum
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 2:
+                                    var cUpdate = readlineSync.question("Enter the new city name: ");
+                                    if (nameRestriction.test(cUpdate) == false && cUpdate.length > 3) {
+                                        console.log("Invalid name!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": addressb.Person[k]["Address"].Street,
+                                            "City": cUpdate,
+                                            "State": addressb.Person[k]["Address"].State,
+                                            "Nationality": addressb.Person[k]["Address"].Nation,
+                                            "Zip": addressb.Person[k]["Address"].Zip,
+                                            "PhoneNum": addressb.Person[k]["Address"].PhoneNum
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 3:
+                                    var stUpdate = readlineSync.question("Enter the new state name: ");
+                                    if (nameRestriction.test(stUpdate) == false && stUpdate.length > 3) {
+                                        console.log("Invalid name!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": addressb.Person[k]["Address"].Street,
+                                            "City": addressb.Person[k]["Address"].City,
+                                            "State": stUpdate,
+                                            "Nationality": addressb.Person[k]["Address"].Nation,
+                                            "Zip": addressb.Person[k]["Address"].Zip,
+                                            "PhoneNum": addressb.Person[k]["Address"].PhoneNum
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 4:
+                                    var nUpdate = readlineSync.question("Enter the new nation name: ");
+                                    if (nameRestriction.test(nUpdate) == false && nUpdate.length > 3) {
+                                        console.log("Invalid name!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": addressb.Person[k]["Address"].Street,
+                                            "City": addressb.Person[k]["Address"].City,
+                                            "State": addressb.Person[k]["Address"].State,
+                                            "Nationality": nUpdate,
+                                            "Zip": addressb.Person[k]["Address"].Zip,
+                                            "PhoneNum": addressb.Person[k]["Address"].PhoneNum
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 5:
+                                    var zUpdate = readlineSync.question("Enter the new zip code: ");
+                                    if (contactRestriction.test(zUpdate) == false) {
+                                        console.log("Invalid number!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": addressb.Person[k]["Address"].Street,
+                                            "City": addressb.Person[k]["Address"].City,
+                                            "State": addressb.Person[k]["Address"].State,
+                                            "Nationality": addressb.Person[k]["Address"].Nation,
+                                            "Zip": zUpdate,
+                                            "PhoneNum": addressb.Person[k]["Address"].PhoneNum
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 6:
+                                    var pUpdate = readlineSync.question("Enter the new phone number: ");
+                                    if (contactRestriction.test(pUpdate) == false || pUpdate.length != 10) {
+                                        console.log("Invalid number!");
+                                        return false;
+                                    }
+                                    var obj = {
+                                        "Name": addressb.Person[k].Name,
+                                        "LastName": addressb.Person[k].LastName,
+                                        "Address": {
+                                            "Street": addressb.Person[k]["Address"].Street,
+                                            "City": addressb.Person[k]["Address"].City,
+                                            "State": addressb.Person[k]["Address"].State,
+                                            "Nationality": addressb.Person[k]["Address"].Nation,
+                                            "Zip": addressb.Person[k]["Address"].Zip,
+                                            "PhoneNum": pUpdate
+                                        }
+                                    }
+                                    addressb.Person[k] = obj;
+                                    save();
+                                    break;
+                                case 7:
+                                    console.log("ThankYou!");
+                                    //address();
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            var update = readlineSync.question("Please enter the index you want to delete: ");
+                            delete addressb.Person[update];
+                            for (var k = 0; k < addressb.Person.length; k++) {
+                                if (addressb.Person[k] == null) {
+                                    addressb.Person.splice(k, 1);
+                                }
+                            }
+                            fs.writeFile('addressBook1.json', JSON.stringify(addressb), 'utf-8', function (err) {
+                                if (err) throw err
+                                console.log('Done!')
+                            })
+                            break;
+                        case 3:
+                            this.sorting();
+                            save();
+                            break;
+                        case 4:
+                            function save() {
+                                fs.writeFile('addressBook1.json', JSON.stringify(addressb), 'utf-8', function (err) {
+                                    if (err) throw err
+                                    console.log('File Saved!!')
+                                })
+                            }
+                            save();
+                            break;
+                        case 5:
+                            console.log("ThankYou!!");
+                            break;
+                        default:
+                            console.log("Please enter valid option");
+                            break;
+                    }
+                }
+            } if (temp == -1) {
+                console.log("Profile unavailable!!Please create new one.");
+            }
+        }
+        else {
+            console.log("No profiles to display!!Please create a new profile!");
+            this.createAddress();
+        }
+    }
+}
+function addressBook() {
+    console.log("*****ADDRESS BOOK*****");
+    console.log("1: Create New Profile");
+    console.log("2: Add Address in Old prfile ")
+    console.log("3: Open Address Book");
+    console.log("4: Exit");
+    var choice = readlineSync.question("Please enter your choice: ");
+    switch (parseInt(choice)) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            console.log("ThankYou!");
+            break;
+        default:
+            console.log("Please enter a valid option!!");
+            addressBook();
+    }
+}
+addressBook();
